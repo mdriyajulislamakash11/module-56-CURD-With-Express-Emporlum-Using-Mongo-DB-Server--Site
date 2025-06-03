7fdb705d2075603045ba8d215502d24b6ae450c8
Akash <hafejmohammadakash@gmail.com>
2025-06-03 18:34:00 +0600

    update cpmplet

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zchez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,25 +29,53 @@ async function run() {
 
     app.get("/coffees", async (req, res) => {
       const cursor = userCollection.find();
-      const result = await cursor.toArray(cursor)
-      res.send(result)
+      const result = await cursor.toArray(cursor);
+      res.send(result);
     });
 
-
+    app.get("/coffees/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
 
     app.post("/coffees", async (req, res) => {
       const newCoffee = req.body;
       console.log("Received Coffee:", newCoffee);
-      const result = await userCollection.insertOne(newCoffee)
-      res.send(result)
+      const result = await userCollection.insertOne(newCoffee);
+      res.send(result);
+    });
+
+    app.put("/coffees/:id", async (req, res) => {
+      const id = req.params.id;
+      const coffee = req.body;
+
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+
+      const updateCoffee = {
+        $set: {
+          name: coffee.name,
+          supplier: coffee.supplier,
+          taste: coffee.taste,
+          chef: coffee.chef,
+          category: coffee.category,
+          details: coffee.details,
+          photo: coffee.photo,
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updateCoffee, options);
+      res.send(result);
     });
 
     app.delete("/coffees/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const result = await userCollection.deleteOne(query)
-      res.send(result)
-    })
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
